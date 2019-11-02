14406e11e64222894ce21e6aaf530083f9f160eb
Anjaneyulu Derangula <anjaneyulu.derangula9@gmail.com>
2019-11-02 09:52:32 +0530

    testing get

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 var config = require('./src/config/config.js');
 
-const port = config.port;
+const port = process.env.PORT || config.port;
+
+//testing
+app.get('/test', (req, res)=>{
+    res.send({message: "your application is running"});
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`application is running on ${port}`);
