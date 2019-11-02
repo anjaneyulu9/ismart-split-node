a551d236ea72281afecff681602380ce7a15954a
Anjaneyulu Derangula <anjaneyulu.derangula9@gmail.com>
2019-11-02 15:13:16 +0530

    header

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ mongoose.connect(config.url, { useNewUrlParser: true, useUnifiedTopology: true }
     console.log("successfully connected to DB");
 }).catch(err =>{
     console.log("problem occured while connecting to the DB");
-})
+});
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    next();
+});
+
 
 
 require('./src/route/userRoute.js')(app);
